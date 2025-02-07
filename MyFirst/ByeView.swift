833e598c0c82a575f771c54925d47c8b955cfbfc
Douglas Jasper <doug.jasper@trios.com>
2025-02-07 11:21:12 -0500

    Changed bye to Bye

---
+++ MyFirst/ByeView.swift
@@ -0,0 +1,18 @@
+//
+//  ByeView.swift
+//  MyFirst
+//
+//  Created by Douglas Jasper on 2025-01-20.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text("Bye Bye, World!")
+    }
+}
+
+#Preview {
+    ByeView()
+}
